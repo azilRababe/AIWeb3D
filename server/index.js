47a9e8794cec4ec3d8e4c6ac6d9df7a54e364d03
azilrababe <azilrababe@gmail.com>
2023-06-15 19:26:46 +0100

    Deployment...

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ app.use(express.json({ limig: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
 
+app.route("/").get((req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
